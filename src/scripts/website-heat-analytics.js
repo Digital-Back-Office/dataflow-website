fdbfe31978abf6443c063f8664d326eb368b6e6d
Shubhanshu Pandey <webdevshubhanshu@gmail.com>
2026-08-04 16:55:20 +0530

    Testing the Website Heat analytics

---
+++ src/scripts/website-heat-analytics.js
@@ -0,0 +1,10 @@
+// Programmatic initialisation
+import "https://heatanalytics.app.dataflow.zone/sdk/analytics.js";
+
+Analytics.init({
+  siteId: "SITE_B7BD3D0E84F8",
+  endpoint: "https://heatanalytics.app.dataflow.zone",
+  flushInterval: 5000,
+  trackMouseMove: true,
+  debug: false
+});
